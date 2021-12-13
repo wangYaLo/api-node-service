80c153dc9829d41ca6a19883caab6e0123278c33
duoyuan <duoyuan7225@163.com>
2021-12-13 10:34:55 +0800

    优化登录和注册

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const fs = require('fs')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const loginRouter = require('./routers/loginRouter')
 const app = express()
 app.use(cors())
-app.use(express.urlencoded())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/login', loginRouter)
 app.get('/', function (req, res) {
-    res.send('123')
+  res.send('123')
 })
 
 app.listen(8888, function () {
-    console.log('node is listen to 8888')
+  console.log('node is listen to 8888')
 })
--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -2,44 +2,44 @@ const express = require('express')
 const router = express.Router()
 const addUser = require('../utils/adduser')
 
-router.post('/signup', function(req, res, next) {
-    addUser(req.body, 'signup').then(() => {
-        next()
-    }).catch((err) => {
-        const data = {
-            code: 'A2030',
-            msg: err
-        }
-        res.send(data)
-    })
-}, function(req, res) {
+router.post('/signup', function (req, res, next) {
+  addUser(req.body, 'signup').then(() => {
+    next()
+  }).catch((err) => {
     const data = {
-        code: '200',
-        msg: '注册成功'
+      code: 'error',
+      msg: err
     }
     res.send(data)
+  })
+}, function (req, res) {
+  const data = {
+    code: 'success',
+    msg: '注册成功'
+  }
+  res.send(data)
 })
 
-router.post('/signin', function(req, res, next) {
-    addUser(req.body, 'signin').then(() => {
-        next()
-    }).catch((err) => {
-        const data = {
-            code: 'A2030',
-            msg: err
-        }
-        res.send(data)
-    })
-}, function(req, res) {
+router.post('/signin', function (req, res, next) {
+  addUser(req.body, 'signin').then(() => {
+    next()
+  }).catch((err) => {
     const data = {
-        code: '200',
-        msg: '登录成功'
+      code: 'error',
+      msg: err
     }
     res.send(data)
+  })
+}, function (req, res) {
+  const data = {
+    code: 'success',
+    msg: '登录成功'
+  }
+  res.send(data)
 })
 
-router.use(function(req, res, next){
-    res.status(404).send('未连接成功')
+router.use(function (req, res, next) {
+  res.status(404).send('未连接成功')
 })
 
 module.exports = router
